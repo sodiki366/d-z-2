a055297a7a2ce492ada95a6633508d4e5b69e1d5
sodiki366 <mark1704kos@gmail.com>
2025-03-01 10:44:14 +0300

    dfhngdfghfgh

---
+++ 2.5.py
@@ -0,0 +1,39 @@
+import random
+from all_colors import *
+import pygame
+
+pygame.init()
+size = (1280, 720)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Моя игра")
+BACKGROUND = WHITE
+screen.fill(BACKGROUND)
+x = 0
+y = 0
+rect_size = 200
+colors = [RED, GREEN, BLUE, YELLOW, CYAN, MAGENTA, GRAY, ORANGE, PINK, BROWN, PURPLE, LIME, NAVY, OLIVE, MAROON, TEAL,
+          SILVER, GOLD]
+
+for i in range(1, 18):
+    rect = pygame.Rect(x, y, rect_size / i, rect_size / i)
+    rect.center = (screen.get_width() // 2, screen.get_height() // 2)
+    color = random.choice(colors)
+
+    pygame.draw.rect(screen, color, rect)
+
+FPS = 60
+clock = pygame.time.Clock()
+timer = 0
+running = True
+while running:
+    # Обработка событий игры
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    #
+    #
+    pygame.display.flip()
+    clock.tick(FPS)
+
+pygame.quit()
+
